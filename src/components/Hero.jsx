7a711edaa8457737d67093963c06b83762cb4e0c
OG-Oluoch <makopiyo45@gmail.com>
2025-01-01 08:19:53 +0300

    added motion and motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,17 +12,17 @@ const Hero = () => {
     <div className='flex flex-wrap'>
         <div className='w-full lg:w-1/2'>
         <div className='flex flex-col items-center lg:items-start'>
-            <motion.h1 variants={container(0)}
+            <motion.h1 variants={container(0.5)}
             initial='hidden'
             animate='visible' 
             className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Gerijek Opiyo</motion.h1>
          <motion.span 
-         variants={container(0)}
+         variants={container(1)}
          initial='hidden'
          animate='visible' 
          className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Software Developer</motion.span>
         <motion.p 
-        variants={container(0)}
+        variants={container(1.5)}
         initial='hidden'
         animate='visible' 
         className='my-2 max-w-xl py-6 font-light tracking-tighter'>
@@ -34,7 +34,11 @@ const Hero = () => {
         <div className='w-full lg:w-1/2 lg:p-8'>
         <div className='flex justify-center'>
 
-            <img src={profilePic} alt="My AI generator image " />
+            <motion.img 
+            initial={{x:100, opacity:0}}
+            animate={{x:0, opacity:1}}
+            transition={{duration:1, delay:1.2}}
+            src={profilePic} alt="My AI generator image " />
         </div>
 
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -7,37 +7,89 @@ import { DiPostgresql } from 'react-icons/di'
 import { SiSpringboot } from 'react-icons/si'
 import { RiNodejsLine } from 'react-icons/ri'
 import { TbBrandLaravel } from 'react-icons/tb'
+import { animate, motion } from 'framer-motion' 
+
+const iconVariants = (duration) => ({
+initial:{y:-10},
+animate:{y:[10,-10],
+    transition: {
+        duration:duration,
+        ease: 'linear',
+        repeat: Infinity,
+        repeatType: 'reverse',
+
+    }
+ }
+
+
+})
 
 const Technologies = () => {
   return <div className='border-b border-neutral-800 pb-24'>
-    <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-    <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+    <motion.h2 
+     whileInView={{opacity:1, x:0}}
+     initial={{opacity:0, x:-100}}
+     transition={{duration:1.5}}
+    className='my-20 text-center text-4xl'>Technologies</motion.h2>
+    <motion.div 
+    whileInView={{opacity:1, x:0}}
+    initial={{opacity:0, x:-100}}
+    transition={{duration:1.5}}
+    className='flex flex-wrap items-center justify-center gap-4'>
+        <motion.div 
+        variants={iconVariants(2.5)}
+        initial='initial'
+        animate='animate'
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
         <RiReactjsLine className='text-7xl text-cyan-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
         < TbBrandLaravel className='text-7xl text-red-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'className='rounded-2xl border-4 border-neutral-800 p-4'>
         <RiNodejsLine className='text-7xl text-green-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'className='rounded-2xl border-4 border-neutral-800 p-4'>
         <SiSpringboot className='text-7xl text-green-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
         < DiPostgresql className='text-7xl text-blue-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'className='rounded-2xl border-4 border-neutral-800 p-4'>
         < TbBrandMysql className='text-7xl text-blue-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
         < RiAngularjsLine className='text-7xl text-red-400'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2.5)}
+         initial='initial'
+         animate='animate'className='rounded-2xl border-4 border-neutral-800 p-4'>
         < TbBrandNextjs className='text-7xl text-white-400'/>
-        </div>
+        </motion.div>
     
-    </div>
+      </motion.div>
     </div>
 }
 
