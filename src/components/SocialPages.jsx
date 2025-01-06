8dd2ff95ceceee1644a0be79f19915cf4442e49f
OG-Oluoch <makopiyo45@gmail.com>
2025-01-07 02:19:44 +0300

    added social media links

---
+++ src/components/SocialPages.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const SocialPages = () => {
+  return (
+    <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+      {/* LinkedIn */}
+      <a
+        href="https://www.linkedin.com/in/gerijek-oluoch/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icon-link"
+       
+      >
+        <FaLinkedinIn />
+      </a>
+
+      {/* GitHub */}
+      <a
+        href="https://github.com/OG-Oluoch"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icon-link"
+       
+      >
+        <FaGithub />
+      </a>
+
+      {/* Twitter */}
+      <a
+        href="https://x.com/the1gerry"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="icon-link"
+       
+      >
+        <FaXTwitter/>
+      </a>
+    </div>
+  );
+};
+
+export default SocialPages;
+
